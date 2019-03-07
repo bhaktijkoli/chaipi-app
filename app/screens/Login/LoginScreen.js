0e823f8ffcaf1a90a4058e9992b8060625ab13e7
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-03-07 14:42:35 +0530

    Added Login Screen

---
+++ app/screens/Login/LoginScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Container, Content, Title} from 'native-base';
+
+class LoginScreen extends Component {
+  componentDidMount() {
+  }
+  render() {
+    return(
+      <Container>
+        <Content>
+          <Title>Welcome</Title>
+        </Content>
+      </Container>
+    )
+  }
+}
+
+export default LoginScreen;
