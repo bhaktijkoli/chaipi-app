908f4ca8598521eec65a9f80b4927bb933cc966a
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-03-13 03:12:51 +0530

    Added firebase to iOS

---
+++ ios/Pods/Target Support Files/Pods-ChaiPaan/Pods-ChaiPaan-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChaiPaan : NSObject
+@end
+@implementation PodsDummy_Pods_ChaiPaan
+@end
---
+++ ios/Pods/Target Support Files/Pods-ChaiPaanTests/Pods-ChaiPaanTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChaiPaanTests : NSObject
+@end
+@implementation PodsDummy_Pods_ChaiPaanTests
+@end
