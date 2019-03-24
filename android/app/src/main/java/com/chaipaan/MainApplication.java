4d67e1aeb43d6acacc597633031faee82e274c79
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-03-24 22:07:20 +0530

    Added Shimmer Component

--- android/app/src/main/java/com/chaipaan/MainApplication.java
+++ android/app/src/main/java/com/chaipaan/MainApplication.java
@@ -3,6 +3,7 @@ package com.chaipaan;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.imagepicker.ImagePickerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new ImagePickerPackage(),
             new RNFirebasePackage(),
             new RNGestureHandlerPackage(),
