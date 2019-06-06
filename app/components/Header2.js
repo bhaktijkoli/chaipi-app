eb31b5d4ea1dea1aac04e96897d3d0339b237848
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-06-07 00:43:35 +0530

    Updated Header2

--- app/components/Header2.js
+++ app/components/Header2.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import { withNavigation } from 'react-navigation';
+import { If } from 'react-if';
 import { Header, Left, Body, Right, Button, Title, Icon } from 'native-base';
 
 import Style from './../styles/style';
 
 class Header2 extends Component {
   render() {
+    console.log("Is First Route", this.props.navigation.isFirstRouteInParent());
     return(
       <Header transparent>
-        <Left>
-          <Button transparent onPress={()=> this.props.navigation.goBack()}>
-            <Icon name='arrow-back' />
-          </Button>
-        </Left>
+        <If condition={!this.props.navigation.isFirstRouteInParent()}>
+          <Left>
+            <Button transparent onPress={()=> this.props.navigation.goBack()}>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+        </If>
         <Body>
           <Title>{this.props.title}</Title>
         </Body>
