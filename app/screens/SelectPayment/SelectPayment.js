e25c3b2d79f169d8ccc74c249277530a2e1eeb91
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-08-12 14:20:35 +0530

    Updated Select Payment for lat lon

--- app/screens/SelectPayment/SelectPayment.js
+++ app/screens/SelectPayment/SelectPayment.js
@@ -45,10 +45,8 @@ class SelectPayment extends Component {
   onProceed(payment) {
     this.setState({process: true})
     let address = this.props.navigation.getParam('address');
-    let {location, house, landmark} = address;
-    let lat = location.x;
-    let lon = location.y;
-    Request.post('/cart/proceed', {payment, location, house, landmark, lat, lon})
+    let {lat, lon, house, landmark} = address;
+    Request.post('/cart/proceed', {payment, house, landmark, lat, lon})
     .then(res => {
       this.setState({process: false})
       Auth.setOrder(res.data);
