ea6b8f2985c1b7c9585c6c1fb827eb254c4a5384
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-08-15 02:35:18 +0530

    Added Recent Orders

--- app/screens/Account/OrderHistory/OrderHistory.js
+++ app/screens/Account/OrderHistory/OrderHistory.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { ScrollView, Image } from 'react-native';
+import { ScrollView, FlatList } from 'react-native';
+import {If, Then, Else} from 'react-if';
 import { Container, Content, Button, ListItem, Left, Right, Body, Icon, Text } from 'native-base';
 
 import Header2 from './../../../components/Header2';
-import ButtonEx from './../../../components/Button';
+import SpinnerBox from './../../../components/SpinnerBox';
+import ButtonEx from './../../../components/Button'
+import Request from './../../../utils/request';;
 import Style from './../../../styles/style';
 
+import OrderItem from './OrderItem';
+
 class OrderHistory extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  }
+  componentDidMount() {
+    Request.get('/order/get').then(res => {
+      console.log(res.data);
+      this.setState({orders: res.data, loading: false});
+    });
+  }
   render() {
     return(
       <Container>
         <Header2 title="Your Orders"/>
-        <ScrollView style={Style.content}>
-
-        </ScrollView>
+        <If condition={this.state.loading}>
+          <Then>
+            <SpinnerBox />
+          </Then>
+          <Else>
+            <ScrollView style={Style.content}>
+              <FlatList
+                data={this.state.orders}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={(item) => {return <OrderItem item={item} navigation={this.props.navigation}/>}}
+                  >
+                </FlatList>
+            </ScrollView>
+          </Else>
+        </If>
       </Container>
     )
   }
---
+++ app/screens/Account/OrderHistory/OrderItem.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Card, CardItem, Text, Button } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import moment from 'moment';
+
+import Request from './../../../utils/request';
+
+class OrderItem extends Component {
+  render() {
+    let order = this.props.item.item;
+    let {shop, products, price} = order;
+    products = JSON.parse(products);
+    let createdAt = moment(order.createdAt, 'YYYY-MM-DD HH:mm:ss');
+    return(
+      <Card>
+        <CardItem header>
+          <Col>
+            <Text>{shop.name}</Text>
+            <Text note numberOfLines={1} style={{fontSize:12}}>{shop.address}</Text>
+            <Text note style={{fontSize:15, paddingTop:10}}>&#8377;{price}</Text>
+          </Col>
+        </CardItem>
+        <CardItem>
+          <Col>
+            <Text>{products.map((el, key) => { return `${el.name}x${el.count}${key!=products.length-1?", ":""}`})}</Text>
+            <Text note style={{fontSize:10}}>{createdAt.format('MMMM Do, h:m A')}</Text>
+            <Button primary bordered style={{marginTop: 10}}>
+              <Text>REORDER</Text>
+            </Button>
+          </Col>
+        </CardItem>
+      </Card>
+    )
+  }
+}
+
+export default OrderItem;
