3c11c2fe440453e55402e392e56e77a84b419d05
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-09-03 03:02:07 +0530

    Updated address

--- app/screens/Account/Address/AddressItem.js
+++ app/screens/Account/Address/AddressItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, Toast } from 'native-base';
+import { View, Card, CardItem, Text, Button, Icon, H3, Toast } from 'native-base';
 
 import Request from './../../../utils/request';
 
@@ -7,8 +7,19 @@ class AddressItem extends Component {
   render() {
     let { item, navigation } = this.props;
     item = item.item;
+    let icon = item.type;
+    if(icon == "home") {
+      icon = "home";
+    } else if(icon == "work") {
+      icon = "briefcase";
+    } else {
+      icon = "location-pin"
+    }
     return(
       <View style={{padding:10}}>
+        <View style={{flexDirection: 'row'}}>
+          <H3><Icon name={icon} type="Entypo"/>&nbsp;{item.type.toUpperCase()}</H3>
+        </View>
         <Text style={{fontSize:18}}>{item.house},{item.landmark},{item.address}</Text>
         <View style={{flexDirection:'row', marginTop: 5, alignItems: 'flex-end'}}>
           <Button bordered small style={{alignItems: "flex-start"}} onPress={this.onDelete.bind(this)}>
